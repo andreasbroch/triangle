91057f91f446be5729e3833de4d06767b2d9721f
Andreas Broch <broch@gmx.net>
2019-01-08 18:29:00 +0100

    Changed readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#Triangle Checker
+# Triangle Challenge
+==================
+See the PDF in the email for discussions about the code.
--- src/triangle/TriangleScalene.java
+++ src/triangle/TriangleScalene.java
@@ -3,6 +3,10 @@ package triangle;
 public class TriangleScalene extends TriangleBase implements TriangleCheckerInterface {
 
 	
+	//Checks that no sides of the triangle are of equal length i. e. scalene
+	//Parameters
+	//a, b and c - the length of the sides of the triangle
+	//returns - True if the triangle is scalene else false
 	@Override
 	public boolean check(float a, float b, float c) {
 		return !compare(a, b) &&
